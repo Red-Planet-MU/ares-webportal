e122ff613a90c7500438518996bda4569d45cfae
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-02-21 16:06:53 -0500

    git

--- app/custom-routes.js
+++ app/custom-routes.js
@@ -2,5 +2,10 @@ export default function setupCustomRoutes(router) {
   // Define your own custom routes here, just as you would in router.js but using 'router' instead of 'this'.
   // For example:
   // router.route('yourroute');
-  
+  this.route('tds-limits');
+  this.route('tds-xp-costs');
+  this.route('tdscombat-gear', { path: '/tdscombat/gear/' });
+  this.route('tdscombat-gear-detail', { path: '/tdscombat/gear/:type/:name' });
+  this.route('tdsskills-abilities', { path: '/tdsskills/abilities' });
+  this.route('tdsskills-scan', { path: '/tdsskills/scan' });
 }
