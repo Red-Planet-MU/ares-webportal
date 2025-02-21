8ae91e2892e6ab15b8df380e0fbc8b774d3393ce
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-02-21 16:39:26 -0500

    testing

--- app/components/chargen-custom-tabs.js
+++ app/components/chargen-custom-tabs.js
@@ -3,5 +3,3 @@ import Component from '@ember/component';
 export default Component.extend({
   tagName: ''
 });
-
-<li class="nav-item"><a data-bs-toggle="tab" class="nav-link" href="#Abilities">Abilities</a></li> 
