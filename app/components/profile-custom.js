5826c4fc211077d2ed0613c600ba12a60193dd97
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-10-24 11:35:48 -0400

    Faraday way

--- app/components/profile-custom.js
+++ app/components/profile-custom.js
@@ -3,10 +3,14 @@ import { action } from '@ember/object';
 
 export default Component.extend({
   tagName: '',
-  
+ selectSerum: false,
+ 
+ @action
+  setSelectSerum(value) {
+    this.set('selectSerum', value);
+  },
   @action
   reloadChar() {
     this.onReloadChar();
   }
-
 });
