e9a14fc70d4cff6156e38b12138a0cf90ab77a11
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-10-24 12:03:51 -0400

    make my files look exactly like profile-custom

--- app/components/char-serums.js
+++ app/components/char-serums.js
@@ -1,40 +1,16 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default Component.extend({
-  flashMessages: service(),
-  gameApi: service(),
-
-  selectSerum: false,
-  
-  @action
-   setSelectSerum(value) {
-     this.set('selectSerum', value);
-   },
-  
+  tagName: '',
+ selectSerum: false,
+ 
+ @action
+  setSelectSerum(value) {
+    this.set('selectSerum', value);
+  },
   @action
   reloadChar() {
     this.onReloadChar();
-  },
-
-  @action
-  getSerum() {
-    let api = this.gameApi;
-         
-    api.requestOne('getSerum', 
-    { 
-      char_id: this.get('char.id'),
-      serum_type: this.get('char.serumType') 
-    }, null)
-    .then( (response) => {
-      if (response.error) {
-        return;
-      }
-      this.flashMessages.success('Serum got!');
-      this.onReloadChar();
-    });
   }
-
-
 });
