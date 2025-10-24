2ba8f142992a20df79c28215cf3ea61c4b497dbf
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-10-24 18:10:10 -0400

    Try adding handle instructions to .js

--- app/components/profile-custom.js
+++ app/components/profile-custom.js
@@ -12,5 +12,21 @@ export default Component.extend({
   @action
   reloadChar() {
     this.onReloadChar();
+  },
+
+  @action
+  getSerum() {
+    let api = this.get('gameApi');
+    api.requestOne('getSerum', {
+      char_id: this.get('char.id'),
+      serum_type: this.get('char.serum_type')
+    }, null)
+    .then( (response) => {
+        if (response.error) {
+            return;
+        }
+      this.flashMessages.success('Serum obtained!');
+      this.reloadChar();
+    });
   }
 });
