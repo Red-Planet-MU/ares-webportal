6d7e1fd742e3e320e0683f444c57610f37f66b66
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-12-11 10:30:58 -0500

    testing AGAIN

--- app/components/profile-demographics.js
+++ app/components/profile-demographics.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    demoIsSongLink: function() {
+        return this.get('demographic') === 'Theme Song Link';
+    }.property('demographic'),
+
+    demoIsSongTitle: function() {
+        return this.get('demographic') === 'Theme Song Title';
+    }.property('demographic')
 });
