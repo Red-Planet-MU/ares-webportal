875ecb60eba4e1567a6f8c953d63826faf95652c
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-12-11 10:51:54 -0500

    testing

--- app/components/profile-demographics.js
+++ app/components/profile-demographics.js
@@ -1,11 +1,4 @@
 import Component from '@ember/component';
 
-Handlebars.registerHelper('ifCond', function(v1, v2, options) {
-        if(v1 === v2) {
-            return options.fn(this);
-        }
-        return options.inverse(this);
-    });
-
 export default Component.extend({
 });
