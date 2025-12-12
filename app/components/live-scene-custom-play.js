c9de1997a79858f91f5eec677d83ac3b86dc0e15
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-12-12 15:52:48 -0500

    starting to add socializer buttons

--- app/components/live-scene-custom-play.js
+++ app/components/live-scene-custom-play.js
@@ -1,5 +1,22 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default Component.extend({
-  tagName: ''
+  tagName: '',
+  gameApi: service(),
+
+  @action 
+    webPalsInvite() {
+    let api = this.get('gameApi');
+    api.requestOne('webPalsInvite', {
+      char_id: this.get('char.id')
+    }, null)
+    .then( (response) => {
+        if (response.error) {
+            return;
+        }
+    });
+  }
+
 });
