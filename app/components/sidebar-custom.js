a9bb18a560d32029df6fff0828ccce5d405d79e3
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-12-17 13:39:27 -0500

    Trying to add web lfrp

--- app/components/sidebar-custom.js
+++ app/components/sidebar-custom.js
@@ -1,5 +1,26 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default Component.extend({
-  tagName: ''
+  gameApi: service(),
+
+  @action
+  reloadChar() {
+    this.reloadChar();
+  },
+
+  @action
+  setLFRP() {
+    let api = this.get('gameApi');
+    api.requestOne('setLFRP', {
+      char_id: this.get('char.id'),
+    }, null)
+    .then( (response) => {
+        if (response.error) {
+            return;
+        }
+      this.reloadChar();
+    });
+  },
 });
