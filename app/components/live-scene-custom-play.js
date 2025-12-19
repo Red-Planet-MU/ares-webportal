4f994fe63fe42a30687e20fb4c444ba1215b024a
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-12-19 12:18:08 -0500

    testing

--- app/components/live-scene-custom-play.js
+++ app/components/live-scene-custom-play.js
@@ -10,7 +10,7 @@ export default Component.extend({
   serums: ['Revitalizer'],
  
   @action
-    selectWebUseSerum(value) {
+    setSelectWebUseSerum(value) {
       this.set('selectWebUseSerum', value);
     },
   
