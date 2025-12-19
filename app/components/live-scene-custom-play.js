8c014f2755a05caa647c176942e8a208fbaacb72
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-12-19 13:29:57 -0500

    typo

--- app/components/live-scene-custom-play.js
+++ app/components/live-scene-custom-play.js
@@ -17,8 +17,7 @@ export default Component.extend({
   @action 
     serumToUseChanged(newSerumToUse) {
       this.set('serumToUse', newSerumToUse)
-    }
-  
+    },  
 
   @action 
     webPalsInvite() {
