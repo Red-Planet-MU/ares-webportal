39f7a8a77d8d8d22c4790b18d535a0be90e6531f
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-12-19 15:33:20 -0500

    testing

--- app/components/live-scene-custom-play.js
+++ app/components/live-scene-custom-play.js
@@ -7,7 +7,7 @@ export default Component.extend({
   gameApi: service(),
   flashMessages: service(),
   selectWebUseSerum: false,
-  serums: [{name: 'Revitalizer'}],
+  serums: [ { name: 'Revitalizer' } ],
  
   @action
     setSelectWebUseSerum(value) {
