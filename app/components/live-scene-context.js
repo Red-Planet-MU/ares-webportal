39a050449d6a5aa124d353bce6e217a2e661feb5
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-01-08 16:56:23 -0500

    formatting and structure for web fortunes

--- app/components/live-scene-context.js
+++ app/components/live-scene-context.js
@@ -1,4 +1,22 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default Component.extend({
+    gameApi: service(),
+
+    @action
+    webGetFortune() {
+    let api = this.get('gameApi');
+    api.requestOne('getFortune', {
+      char_id: this.get('char.id'),
+      char: this.get('char.name'),
+    }, null)
+    .then( (response) => {
+       if (response.error) {
+             return;
+        }
+
+    });
+  },
 });
