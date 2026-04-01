2b2af824602dfd727f4285d6aa5b46fe5d262fad
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-04-01 09:20:18 -0400

    allow roster to check if authenticated

--- app/routes/roster.js
+++ app/routes/roster.js
@@ -1,8 +1,9 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import DefaultRoute from 'ares-webportal/mixins/default-route';
+import AuthenticatedRoute from 'ares-webportal/mixins/authenticated-route';
+//import DefaultRoute from 'ares-webportal/mixins/default-route';
 
-export default Route.extend(DefaultRoute, {
+export default Route.extend(AuthenticatedRoute,  {
     gameApi: service(),
     
     model: function() {
