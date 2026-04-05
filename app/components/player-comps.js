8fbfe7cd3c26515d09ff3c6f338b4acdc8d83326
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-04-05 11:25:08 -0400

    testing testing

---
+++ app/components/player-comps.js
@@ -0,0 +1,55 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+
+export default Component.extend({
+  flashMessages: service(),
+  gameApi: service(),
+  filter: 'All',
+  page: 1,
+
+  resetOnExit: function() {
+    this.set('page', 1);
+  },
+
+  updateCompsList: function() {
+    let api = this.gameApi;
+    api.requestOne('comps', {
+      page: this.page,
+      char_id: this.get('model.id') })
+    .then( (response) => {
+        if (response.error) {
+          return;
+        }
+        this.set('model.comps', response);
+    });
+  },
+
+  @action
+    reloadChar() {
+      this.onReloadChar();
+    },
+    
+  @action
+  save() {
+    let api = this.get('gameApi');
+    api.requestOne('addComp', {
+      char_id: this.get('char.id'),
+      comp_msg: this.get('char.comp_msg')
+    }, null)
+    .then( (response) => {
+        if (response.error) {
+            return;
+        }
+      this.flashMessages.success('Compliment added!');
+      this.reloadChar();
+    });
+  },
+    
+  @action
+  goToPage(newPage) {
+    this.set('page', newPage);
+    this.updateCompsList();
+  }
+
+});
