5f8d99f4c31656bd3c859e68570ae4bc7dcde88d
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-04-22 12:29:45 -0400

    add lfrp modal to Play

--- app/components/play-custom.js
+++ app/components/play-custom.js
@@ -7,8 +7,15 @@ export default Component.extend({
   flashMessages: service(),
   showLFRP: false,
   router: service(),
-  
+  selectLFRP: false,
+  hourOptions: [1, 2, 3],
+  hours: 1,
 
+  @action
+    setSelectLFRP(value) {
+      this.set('selectLFRP', value);
+    },
+  
   @action
   setShowLFRP(value) {
     this.set('showLFRP', value);
@@ -20,17 +27,21 @@ export default Component.extend({
   },
 
 
-  @action
+    @action
   setLFRP() {
     let api = this.get('gameApi');
+    let hours = this.hours;
+    this.set('selectLFRP', false);
     api.requestOne('setLFRP', {
       char_id: this.get('char.id'),
+      hours: hours
     }, null)
     .then( (response) => {
         if (response.error) {
             return;
         }
-      this.flashMessages.success('Set to Looking for RP for 1 hour!');
+      this.flashMessages.success(`Set to Looking for RP for ${hours} hour(s)!`);
+      this.refresh()
     });
   },
 
