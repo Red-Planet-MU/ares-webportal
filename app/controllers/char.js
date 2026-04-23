345f448b7e1c8a6169b5461845d4d5af920d02d9
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-04-23 16:57:15 -0400

    oh god

--- app/controllers/char.js
+++ app/controllers/char.js
@@ -1,6 +1,5 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { render } from '@ember/test-helpers';
 import { pushObject } from 'ares-webportal/helpers/object-ext';
 
 export default Controller.extend({
